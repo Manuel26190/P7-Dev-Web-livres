897a6d3df7334cfc945cc512b60981f807a0e5e3
Manuel Faure <manuelfaure26190@gmail.com>
2023-07-28 16:58:11 +0200

    save

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();//Création de l'applcation express en l'appelant sous forme de variable.
 
+mongoose.connect('mongodb+srv://me7sud:emoroide26@cluster0.lhpisa8.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {//middleware qui gère le CORS
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');//permet d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');//permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');//permet d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
     next();
   });
 
