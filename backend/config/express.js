9996bf41acf618690a5702c18c1e622f8af00c2b
Manuel26190 <manuelfaure26190@gmail.com>
2023-08-03 16:25:19 +0200

    save

--- backend/config/express.js
+++ backend/config/express.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+
 
 module.exports = (app) => {
-  //ce middleware intercepte les requête qui ont un content type json 
-  //et nous met a disposition ce contenu sur l'objet requête dans req.body
-  app.use(express.json());
-  app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');//permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API.
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');//permet d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
-      next();
-  });
-}
+      // Middleware for parsing JSON request bodies
+      app.use(express.json());
+      // Middleware for parsing URL-encoded request bodies
+      app.use(express.urlencoded({ extended: true }));
+      //Middleware CORS qui autorise les requêtes cross-origin de toutes les origines
+      app.use(
+            cors({
+                  'Access-Control-Allow-Origin': '*',
+            })
+      );
+};
 
 
