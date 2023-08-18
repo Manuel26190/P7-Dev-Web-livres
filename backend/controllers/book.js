5fbf139160bd88098de6b008fc589446a79ea138
Manuel Faure <manuelfaure26190@gmail.com>
2023-08-18 10:07:46 +0200

    save

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -172,7 +172,7 @@ async function rateBook(req, res) {
     } catch (error) {
           return res.status(500).json({ message: error.message });
     }
-}
+};
 
 module.exports = {
     getAllBooks,
