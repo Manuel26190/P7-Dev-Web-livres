5ee808e8c3262f60c4929cc4a408330743327bf3
Manuel Faure <manuelfaure26190@gmail.com>
2023-08-18 10:05:02 +0200

    save

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -2,7 +2,6 @@ const Book = require('../models/Book');
 const fs = require('fs');
 const multer = require('multer');
 
-
 async function getAllBooks(req, res, next) {//middleware get pour tous les livres
     try {
         const books = await Book.find({});
@@ -68,66 +67,6 @@ async function addNewBook (req, res, next) {//POST ajouter un nouveau livre.
    .catch(error => { res.status(400).json( { error })})
 };
 
-// async function updateBook(req, res) {
-//     const { bookId } = req.params;
-//     // Check if the bookId parameter is provided
-//     if (!bookId) {
-//           return res.status(404).json({ message: 'Livre non trouvé' });
-//     }
-
-//     try {
-//           // Recherche du livre dans la base de données avec son bookId.
-//           const book = await Book.findOne({ _id: bookId });
-//           let imageUrl = book.imageUrl;
-//           // Vérifier si une nouvelle image est chargée.
-//           // Extraction des détails du livre de la requête body.
-//           if (req.file) {
-//                 // Suppression de l'ancien fichier image
-//                 const oldImagePath = book.imageUrl.split('/').pop(); // Extraction de l'ancien fichier image de l'URL.
-//                 fs.unlink(`images/${oldImagePath}`, (err) => {
-//                       if (err) {
-//                             console.error(
-//                                   "Erreur de suppression de l'ancienne image:",
-//                                   err
-//                             );
-//                       }
-//                 });
-//                 const imagePath =
-//                       req.file.originalname.split('.')[0] + '.' + 'webp';
-//                 imageUrl = `http://localhost:4000/books/images/${imagePath}`;
-//           }
-//           const { title, author, year, genre } = JSON.parse(req.body.book);
-//           const bookUpdate = {
-//                 title,
-//                 author,
-//                 year,
-//                 genre,                
-//           };
-//           // Mise à jour du champ imageUrl si une nouvelle image est téléchargée.
-//           if (imageUrl) {
-//                 bookUpdate.imageUrl = imageUrl;
-//           }
-//           // Mise à jour et récupération document livre mis à jour.
-//           const updatedBook = await Book.findByIdAndUpdate(
-//                 bookId,
-//                 bookUpdate,
-//                 {
-//                       new: true, // L'option dans Mongoose qui renvoie le document mis à jour en tant que résultat de findByIdAndUpdate().
-//                       runValidators: true, // Active l'exécution des validateurs lors de la mise à jour.
-//                 }
-//           );
-//           // Vérification si le livre n'est pas trouvé.
-//           if (!updatedBook) {
-//                 return res.status(404).json({ message: 'Livre non trouvé' });
-//           }
-//           // Renvoyer le document du livre mis à jour en tant que réponse.
-//           return res.status(200).json(updatedBook);
-//     } catch (error) {
-//           // Gestion de toutes les erreurs survenues pendant le processus de mise à jour.
-//           res.status(500).json({ message: error.message });
-//     }
-// }
-
 async function updateBook (req, res) {//PUT modifier les informations d'un livre    
     const bookObject = req.file ? {//Vérification si il y a un champ file dans la requête.
         ...JSON.parse(req.body.book),//je récupère cet objet en parsant la chaine de caractère et en recréant l'URL de l'image.
@@ -192,70 +131,48 @@ async function deleteBook(req, res) {//DEL supprimer un livre
     }
 };
 
-// async function rateBook(req, res) {
-//     const id = req.params.bookId;
-//     const { userId, rating } = req.body;
-//     if (userId !== req.auth.userId) {
-//           return res.status(401).json({ message: 'Unauthorized access' });
-//     }
-//     try {
-//           // Validate rating range
-//           if (rating < 1 || rating > 5) {
-//                 return res.status(400).json({
-//                       message: 'Invalid rating. Rating must be between 1 and 5',
-//                 });
-//           }
-//           // Find the book by its ID
-//           const book = await Book.findById(id);
-//           // Check if the book exists
-//           if (!book) {
-//                 return res.status(404).json({ message: 'Book not found' });
-//           }
-//           // Check if the user has already rated the book
-//           const existingRating = book.ratings.find(
-//                 (r) => r.userId === userId
-//           );
-//           if (existingRating) {
-//                 return res.status(400).json({
-//                       message: 'User has already rated this book',
-//                 });
-//           }
-//           // Add the new rating to the book's ratings array
-//           book.ratings.push({ userId, grade: rating });
-//           // Calculate the new average rating
-//           const totalRatings = book.ratings.length;
-//           const sumGrades = book.ratings.reduce((acc, r) => acc + r.grade, 0);
-//           book.averageRating = (sumGrades / totalRatings).toFixed(1);
-//           // Save the updated book
-//           const updatedBook = await book.save();
-
-//           return res.status(200).json(updatedBook);
-//     } catch (error) {
-//           return res.status(500).json({ message: error.message });
-//     }
-// }
-async function rateBook (req, res) {//POST noter un livre
-    try {
-        res.json({message : "post rate book réussie"})
-    } catch {
-        res.status(400).json({ error: error });
+async function rateBook(req, res) {
+    const id = req.params.bookId;
+    const { userId, rating } = req.body;
+    if (userId !== req.auth.userId) {
+          return res.status(401).json({ message: 'Unauthorized access' });
     }
-};
-// async function addNewBook (req, res) {//POST un livre
-//     try {
-//         res.json({message : "post add book réussie"})
-//     } catch {
-//         res.status(400).json({ error: error });
-//     }
-// };
-// async function updateBook (req, res) {//POST un livre
-//     try {        
-//         res.json({message : "put update book réussie"})
-//     } catch {
-//         res.status(400).json({ error: error });
-//     }
-// };
+    try {
+          // Validate rating range
+          if (rating < 1 || rating > 5) {
+                return res.status(400).json({
+                      message: 'Invalid rating. Rating must be between 1 and 5',
+                });
+          }
+          // Find the book by its ID
+          const book = await Book.findById(id);
+          // Check if the book exists
+          if (!book) {
+                return res.status(404).json({ message: 'Book not found' });
+          }
+          // Check if the user has already rated the book
+          const existingRating = book.ratings.find(
+                (r) => r.userId === userId
+          );
+          if (existingRating) {
+                return res.status(400).json({
+                      message: 'User has already rated this book',
+                });
+          }
+          // Add the new rating to the book's ratings array
+          book.ratings.push({ userId, grade: rating });
+          // Calculate the new average rating
+          const totalRatings = book.ratings.length;
+          const sumGrades = book.ratings.reduce((acc, r) => acc + r.grade, 0);
+          book.averageRating = (sumGrades / totalRatings).toFixed(1);
+          // Save the updated book
+          const updatedBook = await book.save();
 
+          return res.status(200).json(updatedBook);
+    } catch (error) {
+          return res.status(500).json({ message: error.message });
+    }
+}
 
 module.exports = {
     getAllBooks,
@@ -266,3 +183,87 @@ module.exports = {
     deleteBook,
     rateBook,    
 };
+
+// async function addNewBook (req, res) {//POST un livre
+//     try {
+//         res.json({message : "post add book réussie"})
+//     } catch {
+//         res.status(400).json({ error: error });
+//     }
+// };
+
+// async function updateBook (req, res) {//POST un livre
+//     try {        
+//         res.json({message : "put update book réussie"})
+//     } catch {
+//         res.status(400).json({ error: error });
+//     }
+// };
+
+// async function rateBook (req, res) {//POST noter un livre
+//     try {
+//         res.json({message : "post rate book réussie"})
+//     } catch {
+//         res.status(400).json({ error: error });
+//     }
+// };
+
+// async function updateBook(req, res) {
+//     const { bookId } = req.params;
+//     // Check if the bookId parameter is provided
+//     if (!bookId) {
+//           return res.status(404).json({ message: 'Livre non trouvé' });
+//     }
+
+//     try {
+//           // Recherche du livre dans la base de données avec son bookId.
+//           const book = await Book.findOne({ _id: bookId });
+//           let imageUrl = book.imageUrl;
+//           // Vérifier si une nouvelle image est chargée.
+//           // Extraction des détails du livre de la requête body.
+//           if (req.file) {
+//                 // Suppression de l'ancien fichier image
+//                 const oldImagePath = book.imageUrl.split('/').pop(); // Extraction de l'ancien fichier image de l'URL.
+//                 fs.unlink(`images/${oldImagePath}`, (err) => {
+//                       if (err) {
+//                             console.error(
+//                                   "Erreur de suppression de l'ancienne image:",
+//                                   err
+//                             );
+//                       }
+//                 });
+//                 const imagePath =
+//                       req.file.originalname.split('.')[0] + '.' + 'webp';
+//                 imageUrl = `http://localhost:4000/books/images/${imagePath}`;
+//           }
+//           const { title, author, year, genre } = JSON.parse(req.body.book);
+//           const bookUpdate = {
+//                 title,
+//                 author,
+//                 year,
+//                 genre,                
+//           };
+//           // Mise à jour du champ imageUrl si une nouvelle image est téléchargée.
+//           if (imageUrl) {
+//                 bookUpdate.imageUrl = imageUrl;
+//           }
+//           // Mise à jour et récupération document livre mis à jour.
+//           const updatedBook = await Book.findByIdAndUpdate(
+//                 bookId,
+//                 bookUpdate,
+//                 {
+//                       new: true, // L'option dans Mongoose qui renvoie le document mis à jour en tant que résultat de findByIdAndUpdate().
+//                       runValidators: true, // Active l'exécution des validateurs lors de la mise à jour.
+//                 }
+//           );
+//           // Vérification si le livre n'est pas trouvé.
+//           if (!updatedBook) {
+//                 return res.status(404).json({ message: 'Livre non trouvé' });
+//           }
+//           // Renvoyer le document du livre mis à jour en tant que réponse.
+//           return res.status(200).json(updatedBook);
+//     } catch (error) {
+//           // Gestion de toutes les erreurs survenues pendant le processus de mise à jour.
+//           res.status(500).json({ message: error.message });
+//     }
+// }
